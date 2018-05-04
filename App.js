a39a67dfdf1399107a1a79e5c7b0b268171b969d
IgorVasilyev <igorvasilyev@IgorVasilyevs-Mac.local>
2018-05-04 03:04:30 -0700

    Second UI part

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
 
+import ToDo from './ToDo';
+import { ScrollView } from 'react-native-gesture-handler';
+
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
+
+  state = {
+    newToDo: ""
+  };
+
   render() {
+
+    const { newToDo } = this.state;
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"></StatusBar>
         <Text style={styles.title}>Kawai Todo</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+            style={styles.input} 
+            value={newToDo}
+            onChangeText={this._controllNewToDo}
+            placeholder={"New To Do"} 
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
+
+  _controllNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -47,5 +76,11 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25,
   }
 });
